ed109811ff3e148a236d11590325c2b1c8187045
csorn2544 <aue_chanisor@hotmail.com>
2021-02-08 19:03:00 +0700

    Promotion

--- lab2_2.c
+++ lab2_2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    float time,proa=199.00,prob=299.00,fraction,seconds;
+    float time,proa=199.00,prob=299.00;
     char promotion;
     scanf("%c",&promotion);
     scanf("%f",&time);
@@ -11,12 +11,9 @@ int main()
         {
             printf("%.2f",proa);
         }
-        else if(time>200)
+        else
         {
-            time=time-200;
-            seconds=time*60;
-            fraction=seconds*(3.00/60.00);
-            proa=proa+fraction;
+            proa=proa+(((time-200)*60)*(3.00/60.00))+0.40;
             printf("%.2f",proa);
         }
     }
@@ -26,12 +23,9 @@ int main()
         {
             printf("%.2f",prob);
         }
-        else if(time>400)
+        else
         {
-            time=time-400;
-            seconds=time*60;
-            fraction=seconds*(2.00/60.00);
-            prob=prob+fraction;
+            prob=prob+(((time-400)*60)*(2.00/60.00))+0.40;
             printf("%.2f",prob);
         }
     }
